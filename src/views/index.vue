2f0a829849b097174fe7e708dede01a43429c3bb
风度万人迷 <805391114@qq.com>
2024-04-15 19:40:58 +0800

    feat:增加路由跳转测试

--- src/views/index.vue
+++ src/views/index.vue
@@ -2,7 +2,7 @@
  * @Author: Wanko
  * @Date: 2024-04-09 22:02:08
  * @LastEditors: Wanko
- * @LastEditTime: 2024-04-15 17:45:00
+ * @LastEditTime: 2024-04-15 19:40:37
  * @Description: 
 -->
 <script setup>
@@ -13,10 +13,16 @@ const onClick = () => {
 onMounted(() => {
   console.log('🚀 ===>: ', 1111)
 })
+
+const router = useRouter()
+const toHome = () => {
+  router.push('home')
+}
 </script>
 
 <template>
   <div>{{ num }}</div>
   <div></div>
   <button @click="onClick">++</button>
+  <button @click="toHome">跳转home页面</button>
 </template>
